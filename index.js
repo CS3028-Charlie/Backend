1838408bf3ab5c0bc23754d0d5c20a23bb9a0c51
Fraser McCarter <114738535+spxllz@users.noreply.github.com>
2024-11-06 12:43:22 +0000

    Added MongoDB connection

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+require('dotenv').config(); // Load environment variables
+
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = process.env.PORT || 3000; // Default to 3000 locally
+const port = process.env.PORT || 3000;
+
+// MongoDB URI with password substituted
+const MONGODB_URI = "mongodb+srv://test:test@cluster0.jakdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-// Serve static files from "assets" directory
 app.use("/assets", express.static("assets"));
 
-// Start the server
+mongoose.connect(MONGODB_URI, {  // Use MONGODB_URI directly here
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening on port ${port}`);
 });
