af20ad469ea20cfdc441b037f6ff22a058cfde7b
K-Jacob8 <k.jacob.22@abdn.ac.uk>
2024-11-11 01:09:28 +0000

    get card previews, upload card, delete card

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const multer = require("multer");
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,6 +13,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
+
+app.use(express.json())
 
 // MongoDB URI with password substituted
 const MONGODB_URI = "mongodb+srv://test:test@cluster0.jakdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
@@ -27,7 +30,14 @@ mongoose.connect(MONGODB_URI, {  // Use MONGODB_URI directly here
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "assets/templates/card-test")
+    let path = `assets/templates/${req.body.title}`
+
+    // create directory if it doesnt exist
+    if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+
+    cb(null, path)
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname)
@@ -39,17 +49,46 @@ const upload = multer({
   storage: storage
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/get_card_previews', (req, res) => {
+  const cardFolders = fs.readdirSync("assets/templates")
+
+  res.json({
+    cards: cardFolders
+  })
 });
 
 app.post("/upload_card", upload.array("images"), (req, res) => {
-  console.log(req.body);
-  console.log(req.files);
-  
   res.json({
     message: "Success"
   })
+});
+
+app.post("/delete_card", (req, res) => {
+  const dir = req.body.card
+  const path = `assets/templates/${dir}`
+
+  // return if already deleted
+  const cards = fs.readdirSync("assets/templates");
+  if (!cards.includes(req.body.card)) { return res.status(200) }
+
+  const files = fs.readdirSync(path);
+
+  try {
+    // delete directory contents
+    for (const file of files) {
+      fs.unlinkSync(`${path}/${file}`);
+    }
+    // delete directory
+    fs.rmdirSync(path);
+
+    res.status(200)
+
+  } catch (err) {
+    console.error(err)
+
+    res.status(400)
+  }
+
 });
 
 app.use("/assets", express.static("assets"));
